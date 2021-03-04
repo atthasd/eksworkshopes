afa1c5fc3e1ef1b92d0dc18fcd1020ba861ce82a
atthasd <47428135+atthasd@users.noreply.github.com>
2021-03-04 14:14:42 +0700

    Create eksworkshopsh_1.sh

---
+++ eksworkshopsh_1.sh
@@ -0,0 +1,6 @@
+sudo pip install --upgrade awscli && hash -r
+sudo yum -y install jq gettext bash-completion moreutils
+export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
+export InstanceID=$(aws ec2 describe-instances --filters Name=instance-type,Values=t3.small --query Reservations[*].Instances[*].[InstanceId] --output text)
+aws ec2 associate-iam-instance-profile --iam-instance-profile Name=eksworkshop-admin --instance-id $InstanceID
+export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
