2d1aea454302e43e70cd08387ca9a9cd89bd4f95
atthasd <47428135+atthasd@users.noreply.github.com>
2021-03-04 14:48:41 +0700

    Update eksworkshopsh_1.sh

--- eksworkshopsh_1.sh
+++ eksworkshopsh_1.sh
@@ -4,5 +4,5 @@ export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/doc
 export InstanceID=$(aws ec2 describe-instances --filters Name=instance-type,Values=t3.small --query Reservations[*].Instances[*].[InstanceId] --output text)
 aws ec2 associate-iam-instance-profile --iam-instance-profile Name=eksworkshop-admin --instance-id $InstanceID
 export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
-echo "Please do uncheck AWS Managed Security from AWS Setting in Preferences"
+echo "Please do uncheck AWS Managed Security from AWS Setting in Preferences before execute 2nd part of the Script"
 
